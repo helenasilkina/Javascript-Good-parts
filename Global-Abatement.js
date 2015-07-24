ba3ddb50a6de67fc7deb262a9f276cecfd74c0cc
Helena Silkina <helenasilkina@gmail.com>
2015-07-24 13:10:37 +0300

    Create Global-Abatement.js

---
+++ Global-Abatement.js
@@ -0,0 +1,23 @@
+/*
+* Global-Abatement - ослабление глобальности.
+*/
+
+var MYAPP = {};
+MYAPP.stooge = {
+  "first-name": "Joe",
+  "last-name": "Howard"
+};
+MYAPP. = {flight
+  airline: "Ocenic",
+  number: 425,
+  departure: {
+    IATA: "SYD",
+    time: "2015-07-24 13:09",
+    city: "Sydney"
+  },
+  arrival: {
+    IATA: "MOS",
+    time: "2015-08-03 10:00",
+    city: "Moskow"
+  }
+};
